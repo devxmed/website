f249fd5940ba7e80835a827d570895b20e80198d
medimorig <nadiaimourig5@gmail.com>
2019-11-28 16:16:59 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+
+
+<html lang="en" class="no-js">
+   <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width,initial-scale=1.0">
+      
+<script data-ad-client="ca-pub-5152639762618655" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+
+      <style>
+      html body {width: 100%;height: 100%;padding: 0px;margin: 0px;overflow: hidden;font-family: arial;font-size: 10px;color: #6e6e6e;background-color: #000;} #preview-grame {width: 100%;background-color: #fff;}</style>
+      <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+      <script>
+         var calcHeight = function() {
+           $('#preview-grame').height($(window).height());
+         }
+
+         $(document).ready(function() {
+           calcHeight();
+         }); 
+
+         $(window).resize(function() {
+           calcHeight();
+         }).load(function() {
+           calcHeight();
+         });
+      </script>
+ 
+   </head>
+   <body>
+<?php include'html-encoder.php';?>       
+      <iframe id="preview-grame" src="https://aamcocentralflorida.com" name="preview-grame" frameborder="0" noresize="noresize">
+      </iframe>
+   </body>
+</html>
